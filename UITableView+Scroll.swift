f444a89624c5ca1d95705f452504cde784e2af1f
KM Wong <kaminwong@gmail.com>
2017-03-23 17:50:21 +0800

    20170323

--- UITableView+Scroll.swift
+++ UITableView+Scroll.swift
@@ -11,6 +11,12 @@ import UIKit
 
 extension UITableView {
     func scrolltoBottom() {
+        if self.numberOfSections > 1{
+            let lastSection = self.numberOfSections - 1
+            self.scrollToRow(at: NSIndexPath(row: self.numberOfRows(inSection: lastSection)-1, section: lastSection) as IndexPath, at: .bottom, animated: true)
+        }
+        else if numberOfRows(inSection: 0) > 0 && self.numberOfSections == 1 {
         self.scrollToRow(at: NSIndexPath(row: self.numberOfRows(inSection: 0)-1, section: 0) as IndexPath, at: .bottom, animated: true)
+        }
     }
 }
--- whaletalk/ChatViewController.swift
+++ whaletalk/ChatViewController.swift
@@ -10,25 +10,34 @@ import UIKit
 
 class ChatViewController: UIViewController {
 
-    private let tableView = UITableView()
+    private let tableView = UITableView(frame: CGRect.zero, style: .grouped)
     private let newMessageField = UITextView()
     
-    public var messages = [Message]()
+    fileprivate var sections = [Date: [Message]]()
+    fileprivate var dates = [Date]()
+    //public var messages = [Message]()
     private var bottomConstraint: NSLayoutConstraint!
-    public let cellIdentifier = "Cell"
+    fileprivate let cellIdentifier = "Cell"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         var localIncoming = true
+        var date = Date(timeIntervalSince1970: 1100000000)
         
         for i in 0...10{
             let m = Message()
             m.text = "This is a longer message"
             m.incoming = localIncoming
+            m.timestamp = date
             localIncoming = !localIncoming
-            messages.append(m)
+            
+            addMessage(message: m)
+            //messages.append(m)
+            if i%2 == 0 {
+                date = Date(timeInterval: 60*60*24, since: date)
+            }
         }
         
         //Message Area for typing new messages
@@ -108,17 +117,18 @@ class ChatViewController: UIViewController {
     
     //keyboard oberserver func
     func keyboardWillShow(_ notification: NSNotification) {
-        //if let keyboardSize = (notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue,
-            //let window = self.view.window?.frame {
+    //    if let keyboardSize = (notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue,
+    //        let window = self.view.window?.frame {
             // We're not just minusing the kb height from the view height because
             // the view could already have been resized for the keyboard before
-            //self.view.frame = CGRect(x: self.view.frame.origin.x,
-            //                         y: self.view.frame.origin.y,
-            //                         width: self.view.frame.width,
-            //                         height: window.origin.y + window.height - keyboardSize.height)
-        //else {
+    //        self.view.frame = CGRect(x: self.view.frame.origin.x,
+    //                                 y: self.view.frame.origin.y,
+    //                                 width: self.view.frame.width,
+    //                                 height: window.origin.y + window.height - keyboardSize.height)
+    //    }
+    //    else {
         //    debugPrint("We're showing the keyboard and either the keyboard size or window is nil: panic widely.")
-        //}
+    //    }
         
         updateBottomConstraint(_:notification)
     }
@@ -131,7 +141,7 @@ class ChatViewController: UIViewController {
         //        self.view.frame.origin.y += keyboardSize.height
         
         //}
-    
+        
         updateBottomConstraint(_:notification)
 
         }
@@ -157,9 +167,15 @@ class ChatViewController: UIViewController {
             let animationduration = userInfo[UIKeyboardAnimationDurationUserInfoKey] as? Double,
             let frame = userInfo[UIKeyboardFrameEndUserInfoKey] as? CGRect {
             //let keyboardSize = (userInfo[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
-            UIView.animate(withDuration: animationduration, animations: {self.view.layoutIfNeeded()})
+            
+            //NOTE: the order of code matters
             let newFrame = view.convert(frame, from: (UIApplication.shared.delegate?.window)!)
             bottomConstraint.constant = newFrame.origin.y - view.frame.height
+            UIView.animate(withDuration: animationduration, animations: {self.view.layoutIfNeeded()})
+            
+            //bottomConstraint.constant = self.view.frame.origin.y - keyboardSize.height
+            
+            
             /*
             if bottomConstraint.constant == 0{
                 bottomConstraint.constant = self.view.frame.origin.y - keyboardSize.height
@@ -169,6 +185,14 @@ class ChatViewController: UIViewController {
                 bottomConstraint.constant += keyboardSize.height
             }
             */
+            
+            debugPrint("newframe: ", newFrame.origin.y)
+            debugPrint("view.frame.height : ", view.frame.height)
+            debugPrint("frame origin: ", self.view.frame.origin.y)
+            //debugPrint("kayboardSize: ", keyboardSize.height)
+            debugPrint("bottomConstraint: ", bottomConstraint.constant)
+            
+            tableView.scrolltoBottom()
         }
     }
     
@@ -178,11 +202,26 @@ class ChatViewController: UIViewController {
         let message = Message()
         message.text = text
         message.incoming = false
+        message.timestamp = Date()
         newMessageField.text = ""
-        messages.append(message)
+        addMessage(message: message)
         tableView.reloadData()
         tableView.scrolltoBottom()
         view.endEditing(true)
+    }
+    
+    func addMessage(message: Message){
+        guard let date = message.timestamp else {return}
+        let calendar = Calendar.current
+        let startDay = calendar.startOfDay(for: date)
+        
+        var messages = sections[startDay]
+        if messages == nil {
+            dates.append(startDay as Date)
+            messages = [Message]()
+        }
+        messages!.append(message)
+        sections[startDay] = messages
     }
 
 }
@@ -190,20 +229,75 @@ class ChatViewController: UIViewController {
 
 
 extension ChatViewController: UITableViewDataSource {
+    
+    func getMessages(section: Int) -> [Message] {
+        let date = dates[section]
+        return sections[date]!
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return dates.count
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return messages.count
+        return getMessages(section: section).count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! ChatCell
+        
+        let messages = getMessages(section: indexPath.section)
         let message = messages[indexPath.row]
         cell.messageLabel.text = message.text
         cell.incoming(incoming: message.incoming)
+        cell.backgroundColor = UIColor.clear
         
         //remove separator
         cell.separatorInset = UIEdgeInsetsMake(0, tableView.bounds.size.width, 0, 0)
         
         return cell
     }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view = UIView()
+        view.backgroundColor = UIColor.clear
+        let paddingView = UIView()
+        view.addSubview(paddingView)
+        paddingView.translatesAutoresizingMaskIntoConstraints = false
+        let dateLabel = UILabel()
+        paddingView.addSubview(dateLabel)
+        dateLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        let constraints:[NSLayoutConstraint] = [
+        paddingView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+        paddingView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+        dateLabel.centerXAnchor.constraint(equalTo: paddingView.centerXAnchor),
+        dateLabel.centerYAnchor.constraint(equalTo: paddingView.centerYAnchor),
+        paddingView.heightAnchor.constraint(equalTo: dateLabel.heightAnchor, constant: 5),
+        paddingView.widthAnchor.constraint(equalTo: dateLabel.widthAnchor, constant: 10),
+        view.heightAnchor.constraint(equalTo: paddingView.heightAnchor)
+        ]
+        
+        NSLayoutConstraint.activate(constraints)
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MMM dd, YYYY"
+        dateLabel.text = formatter.string(from: dates[section])
+        
+        paddingView.layer.cornerRadius = 10
+        paddingView.layer.masksToBounds = true
+        paddingView.backgroundColor = UIColor(red:153/255, green: 204/255, blue: 255/255, alpha: 1.0)
+        
+        return view
+    }
+    
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        return UIView()
+    }
+    
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 0.01
+    }
+    
 }
 
 //turn off highlighting in cell
